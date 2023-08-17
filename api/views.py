fc340ea3bb77b3cc0fd28627f9cae45fd7b9ed45
Zaman Ehsani <zamanehsani@gmail.com>
2023-08-18 01:14:30 +0400

    update the page size on transite pagination

--- api/views.py
+++ api/views.py
@@ -600,7 +600,7 @@ class BookingViewSet(viewsets.ModelViewSet):
 
 from rest_framework.pagination import PageNumberPagination
 class TransitePagination(PageNumberPagination):
-    page_size = 3
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
 
